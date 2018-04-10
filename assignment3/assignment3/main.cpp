780cbc2284bb46af6ccf4d86105249dc33509086
Nicholas Sunga <nick.sunga95@gmail.com>
2018-04-10 10:26:12 -0700

    rmat z

--- assignment3/assignment3/main.cpp
+++ assignment3/assignment3/main.cpp
@@ -191,6 +191,24 @@ vector<GLfloat> rotation_matrix_y (float theta) {
 // Definition of a rotation matrix along the z-axis by theta degrees
 vector<GLfloat> rotation_matrix_z (float theta) {
     vector<GLfloat> rotate_mat_z;
+    float _sin;
+    float _cos;
+    float _radians = radians(theta);
+    bool sin_zero = fabs(sin(_radians) - 0.0) < numeric_limits<float>::epsilon();
+    bool cos_zero = fabs(cos(_radians) - 0.0) < numeric_limits<float>::epsilon();
+    
+    if (sin_zero) { _sin = 0.0; }
+    else { _sin = sin(_radians); }
+    
+    if (cos_zero) { _cos = 0.0; }
+    else { _cos = cos(_radians); }
+    
+    rotate_mat_z = {
+        _cos, -_sin, 0.0, 0.0,
+        _sin, _cos, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    };
     
     return rotate_mat_z;
 }
